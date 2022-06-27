618efb0d828b9b425b8b0c096e7e7eb36ec9d90b
Your Name <you@example.com>
2022-06-27 15:57:24 -0300

    Alteração de pasta

---
+++ src/pages/Tetes/index.tsx
@@ -0,0 +1,4 @@
+// import { StatusBar } from 'expo-status-bar';
+// import React, { useState, useState } from 'react';
+// import { Button, StyleSheet, Text, View } from 'react-native';
+// import { Container, Header } from './'
