50c587b93b1c39ee27ea20659919fbec97a7f693
Your Name <you@example.com>
2022-06-29 17:27:09 -0300

    Alteraçõa

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,17 +2,18 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import initializeApp from '../../initialize-app.json';
+// import Constants from 'expo-constants';
 
-firebase.initializeApp(initializeApp);
+// firebase.initializeApp(initializeApp);
 
 const db = firebase.firestore();
 const auth = firebase.auth();
 
-if (import.meta.env.DEBUG === 'true') {
-  db.useEmulator(import.meta.env.EMULATOR_HOST, import.meta.env.EMULATOR_FIRESTORE_PORT);
-  auth.useEmulator(`http://${import.meta.env.EMULATOR_HOST}:${import.meta.env.EMULATOR_AUTH_PORT}`);
-  // firebase.firestore.setLogLevel('debug');
-}
+// if (Constants.manifest!.extra!.debug === 'true') {
+//   db.useEmulator(import.meta.env.EMULATOR_HOST, import.meta.env.EMULATOR_FIRESTORE_PORT);
+//   auth.useEmulator(`http://${import.meta.env.EMULATOR_HOST}:${import.meta.env.EMULATOR_AUTH_PORT}`);
+//   // firebase.firestore.setLogLevel('debug');
+// }
 
 export { db, auth };
 
