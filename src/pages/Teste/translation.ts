172f7fa716137e92729adfb1bd056533a46a0e7e
Talulah23 <piepo.andressa@gmail.com>
2022-07-21 14:37:50 -0300

    atualizei arquivo translation

--- src/pages/Teste/translation.ts
+++ src/pages/Teste/translation.ts
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Formik, Form } from 'formik';
 import { I18nManager } from 'react-native';
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
 import { setLocale } from 'yup';
 import { pt } from 'yup-locale-pt'
 
