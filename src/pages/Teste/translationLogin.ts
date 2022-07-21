823f65f4bbfa219fec20d87cf924bb891be53076
LeviLeal264 <mateushenrique.mharaujo@gmial.com>
2022-07-21 16:50:40 -0300

    translation login

--- src/pages/Teste/translationLogin.ts
+++ src/pages/Teste/translationLogin.ts
@@ -4,7 +4,8 @@ import { I18nManager } from 'react-native';
 import * as Localization from 'expo-localization';
 import I18n from 'i18n-js';
 import { setLocale } from 'yup';
-import { pt } from 'yup-locale-pt'
+import { pt } from 'yup-locale-pt';
+import { ru } from 'yup-locale-ru'
 
 
 I18n.translations = {
