d03eb42acbc54d78d2dc1a0282be6a0803ba9aa9
LeviLeal264 <mateushenrique.mharaujo@gmial.com>
2022-07-21 14:41:29 -0300

    Arrumei o erro do styled e no index

--- src/pages/cadastroPage/index.tsx
+++ src/pages/cadastroPage/index.tsx
@@ -1,56 +1,51 @@
 import { useRef } from "react";
-import { Container, Button, Paragraph, TextInput, View } from "./styles";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../hooks/Auth";
+// import { View } from "react-native";
+// import { useNavigate } from "react-router-dom";
+// import { useAuth } from "../../hooks/Auth";
+import { Container, Button, Paragraph, TextInput, View } from "./styles"
 
-function CadastroPage() {
-    
-    const nameRef = useRef<HTMLInputElement | null>(null);
-    const emailRef = useRef<HTMLInputElement | null>(null);
-    const passwordRef = useRef<HTMLInputElement | null>(null);
-    const navigate = useNavigate();
+function LoginPage() {
+    // const emailRef = useRef<HTMLInputElement | null>(null);
+    // const passwordRef = useRef<HTMLInputElement | null>(null);
+    // // const navigate = useNavigate();
 
-    const { signIn, isUserDataPresent } = useAuth();
-    const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (emailRef.current?.value !== undefined && passwordRef.current?.value !== undefined) {
-            signIn('email_and_password', {
-                email: emailRef.current?.value,
-                password: passwordRef.current?.value,
-            }).then(() => {
-                navigate('/');
-            });
-        }
-    }
+    // // const { signIn, isUserDataPresent } = useAuth();
+    // const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    //     e.preventDefault();
+    //     if (emailRef.current?.value !== undefined && passwordRef.current?.value !== undefined) {
+    //         // signIn('email_and_password', {
+    //         //     email: emailRef.current?.value,
+    //         //     password: passwordRef.current?.value,
+    //         // }).then(() => {
+    //         //     // navigate('/');
+    //         // });
+    //     }
+    // }
     
     return (
-        isUserDataPresent ? 
-            
-            // onSubmit={handleOnSubmit} className="principal2"
-            <Container>
-                <View>
-                    <Paragraph nativeID="name">Nome</Paragraph>,
-                    <br />
-                    <TextInput ref={nameRef} type="name" placeholder="Insira seu nome" name="name" required/>
-                    <br/>
-                    <br />
-                    <Paragraph nativeID="email">Email</Paragraph>
-                    <br />
-                    <TextInput ref={emailRef} type="email" placeholder="Insira seu email" name="email" required/>
-                    <br/>
-                    <br />
-                    <Paragraph nativeID="password">Senha</Paragraph>
-                    <br />
-                    <TextInput ref={passwordRef} type="password" placeholder="Insira a senha" name="psw" required/>
-                    <Container>
-                        <Button>Cadastre-se</Button>
-                    </Container>
-                </View>   
-            </Container> 
-            :
-                <Container>
-                </Container>
+        <View></View>
+        // isUserDataPresent ? 
+        //     <Container>
+        //         <View >
+        //             <Paragraph nativeID="email">Username</Paragraph>
+        //             <br />
+        //             <TextInput ref={emailRef} type="email" placeholder="Enter email" name="email" required/>
+        //             <br/>
+        //             <br />
+        //             <Paragraph nativeID="Password">Password</Paragraph>
+        //             <br />
+        //             <TextInput ref={passwordRef} type="password" placeholder="Enter Password" name="psw" required/>
+        //             <br />
+        //             <br />
+        //             <Container2>
+        //                 <Button>Login</Button>
+        //             </Container2>
+        //         </View>
+        //     </Container> 
+        //     :
+        //         <Container>
+        //         </Container>
             );
 }
 
-export default CadastroPage;
+export default LoginPage;
