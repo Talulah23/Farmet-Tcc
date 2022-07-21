b4615f650f31bb8d7b22e3838a36ec0131e075b3
Peituda Metida <ph6944048@gmail.com>
2022-07-21 15:06:03 -0300

    adicione o password e escolhida a curent

--- src/pages/Form/index.tsx
+++ src/pages/Form/index.tsx
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import * as React from 'react';
-import { Formik } from 'formik';
-import * as Yup from 'yup';
-import { pt } from 'yup-locale-pt';
-import { string } from 'yup/lib/locale';
-
-Yup.setLocale(pt);
-
-<<<<<<< HEAD
-const useSchame = Yup.object({
-    name: string().required(),
-    email: string(),
-});
-function Form(){
-=======
-export default function Form(){
-    const useSchame = Yup.object({
-        name: string()
-    });
->>>>>>> main
-
-
-
-
-    return (
-    );
-=======
 import * as React from 'react';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
@@ -46,5 +18,4 @@ export default function Form(){
     return (
         <View></View>
     );
->>>>>>> main
 }
