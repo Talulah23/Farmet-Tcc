661f33efbe0f3292550f3e1e647ac07c955f79af
Peituda Metida <ph6944048@gmail.com>
2022-07-22 16:12:42 -0300

    estilização da tela

--- src/pages/Teste/SelecaoIdioma/styles.native.ts
+++ src/pages/Teste/SelecaoIdioma/styles.native.ts
@@ -13,6 +13,7 @@ export const TouchableOpacity = styled.TouchableOpacity.attrs(() => ({
     font-size: 30px;
     height: 60px;
     width: 100px;
+    margin-left: 550px;
 `;
 // export const Button = styled.Button`
 //     background-color: #199FCA;
