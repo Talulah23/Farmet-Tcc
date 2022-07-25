bec4998ebed148cfba2cb40c6d12dc59869c4d25
LeviLeal264 <mateushenrique.mharaujo@gmial.com>
2022-07-25 14:53:27 -0300

    Tentando fazer as coisas

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,7 +4,7 @@ import 'firebase/compat/firestore';
 import initializeApp from '../../initializeApp.json';
 // import Constants from 'expo-constants';
 
-// firebase.initializeApp(initializeApp);
+firebase.initializeApp(initializeApp);
 
 const db = firebase.firestore();
 const auth = firebase.auth();
@@ -12,7 +12,7 @@ const auth = firebase.auth();
 // if (Constants.manifest!.extra!.debug === 'true') {
 //   db.useEmulator(import.meta.env.EMULATOR_HOST, import.meta.env.EMULATOR_FIRESTORE_PORT);
 //   auth.useEmulator(`http://${import.meta.env.EMULATOR_HOST}:${import.meta.env.EMULATOR_AUTH_PORT}`);
-//   // firebase.firestore.setLogLevel('debug');
+//     firebase.firestore.setLogLevel('debug');
 // }
 
 export { db, auth };
