679877a778396b2bf468416be0c5cd877c217d8d
LeviLeal264 <mateushenrique.mharaujo@gmial.com>
2022-07-25 13:53:29 -0300

    aulka 25/07

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -11,17 +11,17 @@ import I18n from "i18n-js";
 
 const useSchema = object({
     password: string().required(),
-    cnpj: string().test(
-        'test-invalid-cnpj',
-        'cnpj inválido',
-        cnpjValue =>
-            cnpjValue !== undefined && cnpj.isValid(cnpjValue),
-    ),
+    // cnpj: string().test(
+    //     'test-invalid-cnpj',
+    //     'cnpj inválido',
+    //     cnpjValue =>
+    //         cnpjValue !== undefined && cnpj.isValid(cnpjValue),
+    // ),
         
     email: string().email(),
 })
 function LoginPage() {
-    const initialValues = { passaword: '', cnpj: '', email: '' };
+    const initialValues = { passaword: '', email: '' };
 
     const handleOnSubmit = async (
         values: typeof initialValues,
@@ -56,28 +56,9 @@ function LoginPage() {
                     {errors.passaword && touched.passaword ? (
                         <Text>{errors.passaword}</Text>
                     ) : null}
-                    <Text>{I18n.t('cnpj')}</Text>
-                    <TextInput
-                        onChangeText={handleChange('cnpj')}
-                        onBlur={handleBlur('cnpj')}
-                        value={values.cnpj}
-                        isValid={
-                            !(errors.cnpj && touched.cnpj)
-                        }
-                    />
-                    {errors.cnpj && touched.cnpj ? (
-                        <Text>{errors.cnpj}</Text>
-                    ) : null}
                     <Text>{I18n.t('email')}</Text>
                     <TextInput
-                        onChangeText={value => {
-                            handleChange('email')(
-                                mask(
-                                    value,
-                                    '99.9.999/9999-99',
-                                ),
-                            );
-                        }}
+                        onChangeText={handleChange('email')}
                         onBlur={handleBlur('email')}
                         value={values.email}
                         isValid={
@@ -87,11 +68,30 @@ function LoginPage() {
                     {errors.email && touched.email ? (
                         <Text>{errors.email}</Text>
                     ) : null}
-{/* 
+                    // {/* <Text>{I18n.t('cnpj')}</Text>
+//                     <TextInput
+//                         onChangeText={value => {
+//                             handleChange('cnpj')(
+//                                 mask(
+//                                     value,
+//                                     '99.9.999/9999-99',
+//                                 ),
+//                             );
+//                         }}
+//                         onBlur={handleBlur('cnpj')}
+//                         value={values.cnpj}
+//                         isValid={
+//                             !(errors.cnpj && touched.cnpj)
+//                         }
+//                     />
+//                     {errors.cnpj && touched.cnpj ? (
+//                         <Text>{errors.cnpj}</Text>
+//                     // ) : null} */}
+// // {/* 
                     <Button
-                        onPress={() => handleSubmit()}
-                        title={I18n.t('submit')}
-                    /> */}
+                    //     onPress={() => handleSubmit()}
+                    //     title={I18n.t('submit')}
+                    // /> */}
                     <TouchableOpacity 
                     onPress={() => handleSubmit()}
                         title={I18n.t('submit')}>
