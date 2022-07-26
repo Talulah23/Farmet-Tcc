c3b83f53330b39e9133f18d30a86bfb02caf2156
LeviLeal264 <mateushenrique.mharaujo@gmial.com>
2022-07-26 14:41:17 -0300

    Comandinho para derrubar seer4ver

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ import App from './src/App';
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
+
+
+
+// netstat -ano|findstr "PID :5000"
+// taskkill /pid FOUNDPID /F
