07c1e61c7373726af774b2c4e706b550473f2428
LeviLeal264 <mateushenrique.mharaujo@gmial.com>
2022-07-28 15:23:47 -0300

    Auth para atentiocação do App exemplo

---
+++ src/hooks/example/Auth/logic.native.ts
@@ -0,0 +1,54 @@
+import authStatic, {
+    FirebaseAuthTypes,
+} from '@react-native-firebase/auth';
+import {
+    GoogleSignin,
+    User,
+} from '@react-native-google-signin/google-signin';
+import { auth } from '../../utils/firebase/index.native';
+import { i18n } from '../../translations';
+import googleServices from '../../../android/app/google-services.json';
+
+GoogleSignin.configure({
+    webClientId:
+        googleServices.client[0].oauth_client[1].client_id,
+});
+
+export const googleSignIn = () => {
+    return GoogleSignin.signIn().then(({ idToken }) => {
+        const googleCredential =
+            authStatic.GoogleAuthProvider.credential(
+                idToken,
+            );
+
+        auth.languageCode = i18n.locale;
+
+        return auth.signInWithCredential(googleCredential);
+    });
+};
+
+export const emailAndPasswordSignIn = (
+    email: string,
+    password: string,
+) => {
+    return auth.signInWithEmailAndPassword(email, password);
+};
+
+export const onAuthStateChanged = (
+    callback: (user: FirebaseAuthTypes.User | null) => void,
+) => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+        if (user !== null) {
+            return callback(user);
+        }
+        return callback(null);
+    });
+
+    return () => {
+        unsubscribe();
+    };
+};
+
+export const authSignOut = () => {
+    auth.signOut();
+};
---
+++ src/hooks/example/Auth/logic.ts
@@ -0,0 +1,63 @@
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithPopup,
+    onAuthStateChanged as authOnAuthStateChanged,
+    User,
+    signOut,
+} from 'firebase/auth';
+import { i18n } from '../../translations';
+import { auth } from '../../utils/firebase';
+
+export const googleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+
+    auth.languageCode = i18n.locale;
+
+    return signInWithPopup(auth, provider);
+};
+
+export const emailAndPasswordSignUp = (
+    email: string,
+    password: string,
+) => {
+    return createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+    );
+};
+
+export const emailAndPasswordSignIn = (
+    email: string,
+    password: string,
+) => {
+    return signInWithEmailAndPassword(
+        auth,
+        email,
+        password,
+    );
+};
+
+export const onAuthStateChanged = (
+    callback: (user: User | null) => void,
+) => {
+    const unsubscribe = authOnAuthStateChanged(
+        auth,
+        user => {
+            if (user) {
+                return callback(user);
+            }
+            return callback(null);
+        },
+    );
+
+    return () => {
+        unsubscribe();
+    };
+};
+
+export const authSignOut = () => {
+    signOut(auth);
+};
