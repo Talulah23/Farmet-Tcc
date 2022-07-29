35bf3880c60eb2e53c81c755a556d48499482d32
LeviLeal264 <mateushenrique.mharaujo@gmial.com>
2022-07-29 14:42:03 -0300

    Tentando arrumar o googleCredential

--- src/utils/example/logger.ts
+++ src/utils/example/logger.ts
@@ -1,24 +1,24 @@
-import { log, warn, error, info } from 'firebase-functions/logger';
-import { AppError } from 'errors/app-error';
+// import { log, warn, error, info } from 'firebase-functions/logger';
+// import { AppError } from 'errors/app-error';
 
-export default (err: Error | AppError) => {
-  if (err instanceof AppError) {
-    switch (err.registryType) {
-      case 'error':
-        error(err.message, err.toJSON());
-        break;
-      case 'info':
-        info(err.message, err.toJSON());
-        break;
-      case 'warn':
-        warn(err.message, err.toJSON());
-        break;
-      case 'log':
-      default:
-        log(err.message, err.toJSON());
-        break;
-    }
-  } else {
-    log(err.message, err);
-  }
-};
+// export default (err: Error | AppError) => {
+//   if (err instanceof AppError) {
+//     switch (err.registryType) {
+//       case 'error':
+//         error(err.message, err.toJSON());
+//         break;
+//       case 'info':
+//         info(err.message, err.toJSON());
+//         break;
+//       case 'warn':
+//         warn(err.message, err.toJSON());
+//         break;
+//       case 'log':
+//       default:
+//         log(err.message, err.toJSON());
+//         break;
+//     }
+//   } else {
+//     log(err.message, err);
+//   }
+// };
--- src/utils/firebase/index.ts
+++ src/utils/firebase/index.ts
@@ -2,17 +2,15 @@
 // import { initializeApp, ServiceAccount } from 'firebase-admin/app';
 // import { getFirestore } from 'firebase-admin/firestore';
 // import { getAuth } from 'firebase-admin/auth';
-// import { logger } from 'firebase-functions';
+
 
 // try {
-//   // eslint-disable-next-line global-require
 //   const serviceAccountCredentials: ServiceAccount = require('../../../.serviceaccount.json');
 
 //   initializeApp({
 //     credential: credential.cert(serviceAccountCredentials),
 //   });
 // } catch (ex) {
-//   logger.warn('Error on importing service account, this application can only access locally resources', ex);
 
 //   initializeApp({});
 // }
