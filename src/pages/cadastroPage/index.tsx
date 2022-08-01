ec9b758c8d546a9c4412b2b46a51806bfc4ebc9a
LeviLeal264 <mateushenrique.mharaujo@gmail.com>
2022-08-01 14:08:55 -0300

    Deixando organizado

--- src/pages/cadastroPage/index.tsx
+++ src/pages/cadastroPage/index.tsx
@@ -1,29 +1,13 @@
 import { useRef } from "react";
 // import { View } from "react-native";
-// import { useNavigate } from "react-router-dom";
-// import { useAuth } from "../../hooks/Auth";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks/Auth";
 import { Formik } from 'formik';
 import { Styles } from "./styles.native";
 import Logo from "../../assets/logo.png";
 import { Container, TouchableOpacity, Paragraph, TextInput, View, Text, Image } from "./styles"
 
 export default function CadastroPage() {
-    // const emailRef = useRef<HTMLInputElement | null>(null);
-    // const passwordRef = useRef<HTMLInputElement | null>(null);
-    // // const navigate = useNavigate();
-
-    // // const { signIn, isUserDataPresent } = useAuth();
-    // const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    //     e.preventDefault();
-    //     if (emailRef.current?.value !== undefined && passwordRef.current?.value !== undefined) {
-    //         // signIn('email_and_password', {
-    //         //     email: emailRef.current?.value,
-    //         //     password: passwordRef.current?.value,
-    //         // }).then(() => {
-    //         //     // navigate('/');
-    //         // });
-    //     }
-    // }
     
     return (
 
@@ -47,25 +31,41 @@ export default function CadastroPage() {
             );
 }
 
+    // const emailRef = useRef<HTMLInputElement | null>(null);
+    // const passwordRef = useRef<HTMLInputElement | null>(null);
+    // // const navigate = useNavigate();
 
-        // isUserDataPresent ? 
-        //     <Container>
-        //         <View >
-        //             <Paragraph nativeID="email">Username</Paragraph>
-        //             <br />
-        //             <TextInput ref={emailRef} type="email" placeholder="Enter email" name="email" required/>
-        //             <br/>
-        //             <br />
-        //             <Paragraph nativeID="Password">Password</Paragraph>
-        //             <br />
-        //             <TextInput ref={passwordRef} type="password" placeholder="Enter Password" name="psw" required/>
-        //             <br />
-        //             <br />
-        //             <Container2>
-        //                 <Button>Login</Button>
-        //             </Container2>
-        //         </View>
-        //     </Container> 
-        //     :
-        //         <Container>
-        //         </Container>
+    // // const { signIn, isUserDataPresent } = useAuth();
+    // const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    //     e.preventDefault();
+    //     if (emailRef.current?.value !== undefined && passwordRef.current?.value !== undefined) {
+    //         // signIn('email_and_password', {
+    //         //     email: emailRef.current?.value,
+    //         //     password: passwordRef.current?.value,
+    //         // }).then(() => {
+    //         //     // navigate('/');
+    //         // });
+    //     }
+    // }
+
+    // isUserDataPresent ? 
+    //     <Container>
+    //         <View >
+    //             <Paragraph nativeID="email">Username</Paragraph>
+    //             <br />
+    //             <TextInput ref={emailRef} type="email" placeholder="Enter email" name="email" required/>
+    //             <br/>
+    //             <br />
+    //             <Paragraph nativeID="Password">Password</Paragraph>
+    //             <br />
+    //             <TextInput ref={passwordRef} type="password" placeholder="Enter Password" name="psw" required/>
+    //             <br />
+    //             <br />
+    //             <Container2>
+    //                 <Button>Login</Button>
+    //             </Container2>
+    //         </View>
+    //     </Container> 
+    //     :
+    //         <Container>
+    //         </Container>
