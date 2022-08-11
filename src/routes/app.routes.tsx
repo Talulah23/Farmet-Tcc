9299d10e24a68b8a16d703981953bb7536d59f6d
LeviLeal264 <mateushenrique.mharaujo@gmail.com>
2022-08-11 14:56:01 -0300

    Tirando o menu

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -11,8 +11,33 @@ import FeedUser from 'pages/LayoutFeedUser';
 const AppRoutes: FC<PropsGroup> = function ({ Drawer }: PropsGroup) {
     return (
         <Drawer.Group>
-            <Drawer.Screen name="Login" component={Login} />
-            <Drawer.Screen name="Cadastro" component={Cadastro} />
+            {/* <Drawer.Screen
+                name='Tela Inicial'
+                componet= {StartScren}
+                options={{
+                    header: false,
+                    gestureHandleProps:{
+                        enable: false,
+                    },
+                }}
+            /> */}
+            <Drawer.Screen 
+                name="Login" 
+                component={Login}
+                // options={{
+                //     headerShown: false,
+                //     gestureHandleProps:{
+                //         enable: false,
+                //     },
+                // }}
+                />
+            <Drawer.Screen 
+                name="Cadastro" 
+                component={Cadastro}
+                // options={{
+                //     drawerItemStyle: { heigth: 0},
+                // }}
+            />
             <Drawer.Screen name="EscolhaConta" component={EscolhaConta} />
             <Drawer.Screen name="EscolhaProfissão" component={EscolhaProfissao} />
             <Drawer.Screen name="InformaçãoProfissional" component={InformacaoProfissional} />
